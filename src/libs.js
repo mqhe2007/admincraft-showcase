fb21789f812233a194c6f96fdb50b235cc15e16d
青衿 <122274389@qq.com>
2019-04-17 00:29:57 +0800

    调整依赖

--- src/libs.js
+++
@@ -1,2 +0,0 @@
-// 配置本地依赖清单
-module.exports = ['libs/prism.css', 'libs/prism.js']
--- src/module-init.js
+++ src/module-init.js
@@ -1,16 +1,8 @@
 import routes from './router/routes'
 import storeModule from './store/storeModule'
-import Prism from 'prismjs'
+import 'prismjs'
 import 'prismjs/themes/prism.css'
-// import libs from './libs'
 let moduleName = require('../package.json').name
-// let i, moduleServerUrl
-// if (
-//   (i = window.document.currentScript) &&
-//   (i = i.src.match(/(.+\/)[^/]+\.js$/))
-// ) {
-//   moduleServerUrl = i[1]
-// }
 export default app => {
   // 注册路由
   app.$addRoutes(routes, () => {})
@@ -21,11 +13,6 @@ export default app => {
   // 注册菜单
   app.$addMenus(routes, () => {})
 
-  // if (libs.length > 0) {
-  //   // 注册libs
-  //   let getRemoteLibUrlList = libs => libs.map(item => moduleServerUrl + item)
-  //   app.$addRemoteLib(getRemoteLibUrlList(libs))
-  // }
   console.log(
     `%c${moduleName}模块加载完成`,
     'background: #4192d9; padding: 5px; color: #fff; border-radius: 5px'
